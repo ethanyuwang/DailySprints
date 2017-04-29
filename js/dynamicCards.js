7c6effec9ee5af14c7f819e40f79d0743107948e
Ethan Wang <ethanyuwang@gmail.ucsb.edu>
2017-04-28 22:12:20 -0700

    time duration can be set through new card interface

--- js/dynamicCards.js
+++ js/dynamicCards.js
@@ -1,17 +1,44 @@
+
+
 /*--------------------------------------Dynamic Cards----------------------------*/
-function card(taskName, duration, notes) {
-    this.duration = duration;
+function card(taskName, duration_hours, duration_minutes, notes) {
     this.taskName = taskName;
+    this.duration_hours = duration_hours;
+    this.duration_minutes = duration_minutes;
     this.notes = notes;
 }
 
 card.prototype.addCols = function() {
     var myCol = $('<div class="col-sm-2 col-md-2 sprintCard"></div>');
-    this.id = Math.floor(Math.random()*20);
-    var myPanel = $('<div class="card" id="'+this.id+'Panel"> <div class="card-block"> <input class="form-control" type="text" id="taskPanel#'+this.id+'" value="'+this.taskName+'"/> </div><div class="card-block"> <input type="text" id="duration" name="duration"> </div><div class="card-block card-notes-section"> <div class="form-group"> <textarea class="form-control" id="notesPanel#'+this.id+'" rows="4">'+this.notes+'</textarea> </div></div><div class="card-block card-buttons-section"> <div class="row"> <button type="button" class="btn card-buttons close" data-target="#'+this.id+'Panel" data-dismiss="alert"> <i class="fa fa-remove"></i> </button> </div></div></div>')
+    this.id = new Date().getUTCMilliseconds();
+    var myPanel = $('<div class="card" id="'+this.id+'Panel"> <div class="card-block"> <input class="form-control" type="text" id="'+this.id+'taskPanel" value="'+this.taskName+'"/> </div><div class="card-block"> <input type="text" id="'+this.id+'duration" name="duration"> </div><div class="card-block card-notes-section"> <div class="form-group"> <textarea class="form-control" id="'+this.id+'" rows="4">'+this.notes+'</textarea> </div></div><div class="card-block card-buttons-section"> <div class="row"> <button type="button" class="btn card-buttons close" data-target="#'+this.id+'Panel" data-dismiss="alert"> <i class="fa fa-remove"></i> </button> </div></div></div>')
     myPanel.appendTo(myCol);
     myCol.insertBefore('#newCardEditor');
 
+    //initialize timepicker for each new card and assign unique ids for hours and minutes sections
+    $("#"+this.id+"duration").durationPicker({
+        hours: {
+            label: "h",
+            min: 0,
+            max: 24,
+            id: "hours"+this.id
+        },
+        minutes: {
+            label: "m",
+            min: 0,
+            max: 59,
+            id: "minutes"+this.id
+        },
+        classname: 'form-control',
+        type: 'number',
+        responsive: true
+    });
+    //set time for each new card
+    document.getElementById("duration-hours"+this.id).value = this.duration_hours;
+    document.getElementById("duration-minutes"+this.id).value = this.duration_minutes;
+
+
+    //on close button
     $('.close').on('click', function(e){
       e.stopPropagation();  
           var $target = $(this).parents('.col-sm-2');
@@ -24,7 +51,7 @@ card.prototype.addCols = function() {
 
 
 $('#btnGen').click(function(){
-    var newCard = new card($('#taskPanel').val(), $('#duration').val(), $('#notesPanel').val());
+    var newCard = new card($('#taskPanel').val(), $('#duration-hours').val(), $('#duration-minutes').val(), $('#notesPanel').val());
     newCard.addCols();
     $('#taskPanels').attr("value") = "";
     //addCols($('#taskPanels').val(), $("#duration").val(), $('textarea#notesPanels').val());
@@ -40,3 +67,5 @@ function init() {
     $('#masthead').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
     //body.style.backgroundImage = 'url(img/background/' + images[Math.floor(Math.random() * images.length)] + ')';
 }
+
+
