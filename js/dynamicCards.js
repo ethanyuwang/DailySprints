977259eb4b0667e7e12c39189ce0887efe3d8738
Ethan Wang <ethanyuwang@gmail.ucsb.edu>
2017-05-04 00:50:26 -0700

    firebase implemented, before testing
    implemented but not functioning yet

--- js/dynamicCards.js
+++ js/dynamicCards.js
@@ -1,21 +1,34 @@
-//var sprintCards = new Firebase('https://sprints-for-the-day.firebaseio.com/sprintCards');
- 
 /*--------------------------------------Global Varibales----------------------------*/
 var pausing = false;
-var secondCounter = 0;
+var cardSuite = {};
+userId = "Ethan"
+
+/*--------------------------------------firebase----------------------------*/
+var database = firebase.database();
+
+var SprintCards = firebase.database().ref('posts/' + userId);
+SprintCards.on('value', function(snapshot) {
+	cardSuite = snapshot.val();
+	populateAllCards();
+});
+
+
 
 /*--------------------------------------Dynamic Cards----------------------------*/
-function card(taskName, duration_hours, duration_minutes, notes) {
+function card(taskName, duration_hours, duration_minutes, duration_seconds, notes) {
     this.taskName = taskName;
     this.duration_hours = duration_hours;
     this.duration_minutes = duration_minutes;
     this.notes = notes;
+    this.duration_seconds = duration_seconds;
 }
 
 card.prototype.addCols = function() {
     this.id = new Date().getUTCMilliseconds();
+    //Add current card to cardSuit
+    cardSuite[this.id] = this
     var myCol = $('<div class="col-sm-2 col-md-2 sprintCard mx-auto" id="'+this.id+'"></div>');
-    var myPanel = $('<div class="card" id="'+this.id+'Panel"> <div class="card-block"> <input class="form-control" type="text" id="'+this.id+'taskPanel" value="'+this.taskName+'"/> </div><div class="card-block"> <input type="text" id="'+this.id+'duration" name="duration"> </div><div class="card-block card-notes-section"> <div class="form-group"> <textarea class="form-control" id="'+this.id+'" rows="4">'+this.notes+'</textarea> </div></div><div class="card-block card-buttons-section"> <div class="row"> <button type="button" class="btn card-buttons close" data-target="#'+this.id+'Panel" data-dismiss="alert" id="'+this.id+'close"> <i class="fa fa-remove"></i> </button> </div></div></div>')
+    var myPanel = $('<div class="card" id="'+this.id+'Panel"> <div class="card-block"> <input class="form-control" type="text" id="'+this.id+'taskPanel" value="'+this.taskName+'"/> </div><div class="card-block"> <input type="text" id="'+this.id+'duration" name="duration"> </div><div class="card-block card-notes-section"> <div class="form-group"> <textarea class="form-control" id="'+this.id+'noets" rows="4">'+this.notes+'</textarea> </div></div><div class="card-block card-buttons-section"> <div class="row"> <button type="button" class="btn card-buttons close" data-target="#'+this.id+'Panel" data-dismiss="alert" id="'+this.id+'close"> <i class="fa fa-remove"></i> </button> </div></div></div>')
     myPanel.appendTo(myCol);
     myCol.insertBefore('#newCardEditor');
 
@@ -48,7 +61,39 @@ card.prototype.addCols = function() {
           var $target = $(this).parents('.col-sm-2');
           $target.hide(function(){ $target.remove(); });
     });
+
+card.prototype.updateInfo = function(taskName, duration_hours, duration_minutes, duration_seconds, notes) {
+    this.taskName = taskName;
+    this.duration_hours = duration_hours;
+    this.duration_minutes = duration_minutes;
+    this.duration_seconds = duration_seconds;
+    this.notes = notes;
+}
 };
+
+//TODO: probably not the best approach to save all for each snapshot but will do for now
+function updateAll() {
+	for(id in cardSuite) {
+		var hours = document.getElementById("duration-hours"+id).value;
+	    var minutes = document.getElementById("duration-minutes"+id).value;
+	    var seconds = cardSuite[id].duration_seconds;
+	    var taskName = document.getElementById(id+"taskPanel").value;
+	    var notes = document.getElementById(id+"noets").value;
+	    cardSuite[id].updateInfo(taskName, hours, minutes, seconds, notes);
+	}
+
+	//TODO: fix order of cards when save to data base
+	firebase.database().ref('users/' + userId).set({
+        cards: cardSuite
+    });
+}
+
+function populateAllCards() {
+	for(id in cardSuite) {
+		var newCard = new card(cardSuite[id].taskName, cardSuite[id].duration_hours, cardSuite[id].duration_minutes, cardSuite[id].duration_seconds, cardSuite[id].notes)
+        newCard.addCols();
+	}
+}
 
 /*--------------------------------------button for generating new cards----------------------------*/
 $('#btnGen').click(function(){
@@ -57,7 +102,7 @@ $('#btnGen').click(function(){
     var task = $('#taskPanel').val();
     var notes = $('#notesPanel').val();
     if ((hours!=0||minutes!=0)&&(task!=""||notes!="")) {
-    var newCard = new card(task, hours, minutes, notes);
+    var newCard = new card(task, hours, minutes, 1, notes);
         newCard.addCols();
         //reset values after creating new card
         $('#taskPanel').val("");
@@ -85,38 +130,43 @@ $('#btnPause').click(function(){
 
 /*--------------------------------------Update the count down every 1 minute----------------------------*/ 
 setInterval(function() {
-    //increment counter, affected by pausing as well
-    if (!pausing) secondCounter++;
+	//find first card
+	var firstCard = $( ".sprintCard" ).first();
+	if (firstCard!=null) {
+		var id = firstCard.attr('id');
+	    var hours = document.getElementById("duration-hours"+id).value;
+	    var minutes = document.getElementById("duration-minutes"+id).value;
+	    var seconds = cardSuite[id].duration_seconds;
+	    //decrement counter, affected by pausing as well
+	    if (!pausing) {
 
-    //get the first card's id
-    //var id = $( "sprintCard:first" ).attr('id');
-    if (!pausing&&(secondCounter==59)) {
-        var firstCard = $( ".sprintCard" ).first();
-        if (firstCard!=null) {
-            var id = firstCard.attr('id');
-
-            var hours = document.getElementById("duration-hours"+id).value;
-            var minutes = document.getElementById("duration-minutes"+id).value;
-
-            if (minutes!=0) {
-                minutes-=1;
+	    	if (seconds!=0) {
+                seconds-=1;
             }
             else {
+                seconds=59;
+                minutes-=1;
+            }
+            if (minutes==0) {
                 minutes=59;
                 hours-=1;
             }
 
+            //TODO: off by one minute here, to fix, display seconds or not?
             if (hours<=0&&minutes<=0) {
                 $('#'+id+'close').click();
+                cardSuite.splice(id, 1);
             }
             else {
                 document.getElementById("duration-hours"+id).value = hours;
                 document.getElementById("duration-minutes"+id).value = minutes;
             }
+            //update seconds in the instance
+            cardSuite[id].duration_seconds = seconds;
         }
-    }
-    //resatrt counter
-    if (secondCounter>=59) secondCounter = 0;
+	    //TODO: save cards #not the best place but will do for now
+	    updateAll();
+	}
 }, 1000);
 
 
